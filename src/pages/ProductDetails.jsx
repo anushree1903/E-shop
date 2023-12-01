0f0c771c333ecfb5be2037d9b82306870f09d8a5
anushree <anushree9019@gmail.com>
2023-12-01 18:42:30 +0530

    final

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { useParams } from "react-router-dom";
-import { CartContext } from "../Contexts/CartContext";
+import { CartContext } from "../contexts/CartContext";
 import { ProductContext } from "../Contexts/ProductContext";
 import { motion, useAnimation, useInView } from "framer-motion";
 import { useEffect, useRef } from "react";
